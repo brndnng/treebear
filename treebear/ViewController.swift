0aea0c0d6e43f49c343325d1cd963836be9e1766
Ricky <asonu0917@users.noreply.github.com>
2017-11-28 14:10:29 +0800

    Revert "dummy"
    This reverts commit 6bab255b2a786bc02719abb1c92dab6169f29f38.

--- treebear/ViewController.swift
+++ treebear/ViewController.swift
@@ -8,12 +8,10 @@
 
 import UIKit
 import MapKit
-import Hero
-class mapViewController: UIViewController,MKMapViewDelegate {
+class ViewController: UIViewController,MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var searchBar: UISearchBar!
-//    @IBOutlet var pan2AR: UIScreenEdgePanGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,23 +20,11 @@ class mapViewController: UIViewController,MKMapViewDelegate {
         mapView.showsUserLocation = true
         mapView.alpha = 1
         view.addSubview(mapView)
-//        pan2AR = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(switch2AR(gestureRecognizer:)))
-        view.addGestureRecognizer(pan2AR)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    @objc func switch2AR(gestureRecognizer:UIScreenEdgePanGestureRecognizer){
-        switch pan2AR.state {
-        case .began:
-            //hero_replaceViewController(with:arViewController)
-            performSegue(withIdentifier: "ToAR", sender: self)
-        default:
-            break
-        }
     }
 
 
